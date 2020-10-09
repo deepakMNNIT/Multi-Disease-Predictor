ac5452f511bed72ae82a0d5b222740c9f8261b52
deepak <34981370+deepakMNNIT@users.noreply.github.com>
2020-10-09 11:34:34 +0530

    Update model.py

--- py_files/model.py
+++ py_files/model.py
@@ -22,24 +22,24 @@ class Diabetes(Base):
         procedure = Column(String)
         lastupdatedat = Column(sqlalchemy.DateTime(timezone=False), server_default=sqlalchemy.sql.func.now())
     
-class Liver(Base):
-        __tablename__ = 'liver'
-        __table_args__ = {'extend_existing': True} 
-        id = Column(Integer, primary_key=True)
-        patientid = Column(String)
-        age = Column(Integer)
-        totalbilirubin = Column(String)
-        directbilirubin = Column(String)
-        alkalinephosphotase = Column(String)
-        alamineaminotransferase = Column(String)
-        aspartateaminotransferase = Column(String)
-        totalprotiens = Column(String)
-        albumin = Column(String)
-        albuminandglobulinratio = Column(String)
-        gender = Column(String)
-        result = Column(String)
-        conf = Column(String)
-        lastupdatedat = Column(sqlalchemy.DateTime(timezone=False), server_default=sqlalchemy.sql.func.now())
+# class Liver(Base):
+#         __tablename__ = 'liver'
+#         __table_args__ = {'extend_existing': True} 
+#         id = Column(Integer, primary_key=True)
+#         patientid = Column(String)
+#         age = Column(Integer)
+#         totalbilirubin = Column(String)
+#         directbilirubin = Column(String)
+#         alkalinephosphotase = Column(String)
+#         alamineaminotransferase = Column(String)
+#         aspartateaminotransferase = Column(String)
+#         totalprotiens = Column(String)
+#         albumin = Column(String)
+#         albuminandglobulinratio = Column(String)
+#         gender = Column(String)
+#         result = Column(String)
+#         conf = Column(String)
+#         lastupdatedat = Column(sqlalchemy.DateTime(timezone=False), server_default=sqlalchemy.sql.func.now())
         
 class Diabetes_Procedures(Base):
         __tablename__ = 'procedures'
